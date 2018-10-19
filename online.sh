47e03ac8d6de953c1d9c3b876788fb9a7d58f023
Maartje Eyskens <maartje@eyskens.me>
2018-10-19 20:59:15 +0200

    exit if lynx errored

--- online.sh
+++ online.sh
@@ -5,7 +5,13 @@ if [ -z "${1}" ]; then
     exit
 fi
 
-result=$(lynx --dump "https://www.online.net/en/server-dedicated/${1}"| grep -c "Victim of its success")
+page=$(lynx --dump "https://www.online.net/en/server-dedicated/${1}")
+if [ "$?" != "0" ]
+    then
+    exit 0
+fi
+
+result=$(echo "$page" | grep -c "Victim of its success")
 if [ "$result" == "0" ]
     then
 	echo "@maartje @rafa Dedibox in stock! Quick https://www.online.net/en/server-dedicated/${1}" | slacktee.sh -m link_names
